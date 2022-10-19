463418d084856875b0c20e2c5dbf747058bb410d
Chien52hz <116178504+Chien52hz@users.noreply.github.com>
2022-10-20 01:07:01 +0700

    ok

---
+++ README.md
@@ -0,0 +1,18 @@
+# Tặng người yêu
+## _Một điều nho nhỏ cho người yêu_
+Link Demo: https://binncodee.github.io/Lovee_Camm_Lyy/
+
+## Tác giả: Trần Hoàng Hải
+
+**Facebook**:https://www.facebook.com/Iraneeeeman/</a>
+
+## Cách sử dụng:
+
+Nếu bạn muốn dùng web này theo cách của bạn hoặc muốn đưa lên host riêng của bạn để gửi cho người thầm thương nhớ của bạn thì hãy đọc hướng dẫn sử dụng này.
+
+**Note**: Nếu các bạn không thích động tay động chân thì dùng luôn link demo ở phía trên.
+Hướng dẫn mình sẽ làm sau :))
+
+Chúc các bạn thành công!
+
+Nếu như thấy hay thì hãy vote STAR repo này và theo dõi mình trên facebook nhé. Cảm ơn!
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Xin Chào bé Trâm Cute',
+    introDesc: `Chẳng là nay anh có điều muốn hỏi em về chuyện của chúng mình. Hôm nay là 20/10 anh muốn tạo bất ngờ cho em 💌 . Dù thế nào cũng phải trả lời cho thật lòng đấy nhá 😘`,
+    btnIntro: 'Dạaaaa ❤',
+    title: 'Em yêu anh có phải không 😘😘',
+    desc: 'Trên thế giới có hơn 7 tỷ người. Giữa hơn 7 tỷ người đó anh đã tìm thấy em... Em không phải là người đầu tiên anh yêu nhưng anh mong em sẽ là người cùng anh đi một khoảng thời gian thật dài. Chúng mình quen nhau có thể là chưa lâu cũng chỉ mới hơn hai tháng nhưng anh nghĩ là chúng mình cũng đủ thời gian để hiểu rằng đối phương đều có tình cảm với mình. Nên hôm nay nhân dịp này anh muốn nói... Anh Yêu Em💗',
+    btnYes: 'Tau Yêu Mi vãi~~💗',
+    btnNo: 'Tau nỏ thích miii😣',
+    question: 'Em có thể gửi lời thì thầm của em cho anh qua cái này nhó',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì là anh xinh gái, tốt bụng và đặc biệt là yêu em rất nhiều, kiểu như Yêu say đắm anh đóa hihi.',
+    mess: 'Cảm ơn em đã chọn Yêu anh và ở lại với anh trong những giây phút chúng mình cãi nhau... Chúng mình hãy là nơi tựa vào của nhau mỗi lúc mệt mỏi và bên nhau lúc khó khăn, yên bình và cả tất cả những điều gì chúng ta sẽ cùng trải qua trong cuộc sống này nhé. Anh Yêu Em <3',
+    messDesc: 'Hãy để thanh xuân chúng mình có nhau💗',
+    btnAccept: 'Zạaaaaa anh <3, ',
+    messLink: 'https://www.facebook.com/52hz.2004/'
+}
